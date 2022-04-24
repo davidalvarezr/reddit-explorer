e1021f9cc772d504118e687e278bf50ec22d31c1
David Alvarez <devdav@pm.me>
2022-04-24 14:33:19 +0200

    getSubredditIterator works

---
+++ tests/getSubreddit.test.ts
@@ -0,0 +1,28 @@
+import { createRedditClient, SortingMethod } from "../src"
+import { secrets } from "../src/secrets"
+import { RedditClient } from "../src/types/RedditClient"
+
+let client: RedditClient
+
+beforeAll(() => {
+    client = createRedditClient({ clientId: secrets.clientId, secret: secrets.secret })
+})
+
+test("Should get subreddit with sr_detail", async () => {
+    const res = await client.getSubreddit({
+        sortMethod: SortingMethod.Top,
+        name: "news",
+        sr_detail: true,
+    })
+
+    expect(Object.keys(res.data.children[0].data)).toContain("sr_detail")
+})
+
+test("Should get subreddit without sr_detail", async () => {
+    const res = await client.getSubreddit({
+        sortMethod: SortingMethod.Top,
+        name: "news",
+    })
+
+    expect(Object.keys(res.data.children[0].data)).not.toContain("sr_detail")
+})
