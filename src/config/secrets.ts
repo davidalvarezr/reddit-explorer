25779c61cf340df3dc541220e46d4b077e0435f8
David Alvarez <devdav@pm.me>
2022-04-24 20:06:19 +0200

    show secrets

---
+++ src/config/secrets.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv"
+
+config()
+
+// This can be used in Postman as "Basic Auth":
+// clientId --> Username
+// secret   --> Password
+export const secrets = {
+    clientId: process.env.REDDIT_CLIENT_ID ?? "",
+    secret: process.env.REDDIT_SECRET ?? "",
+}
